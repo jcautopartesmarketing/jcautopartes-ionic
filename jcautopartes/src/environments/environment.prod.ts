3811c28e2da04cbfe5d7fca6235418168a2a9d86
lidertech.net <lidertech.net@gmail.com>
2024-10-16 14:18:22 +0000

    Instalacion angular cli

--- jcautopartes/src/environments/environment.prod.ts
+++ jcautopartes/src/environments/environment.prod.ts
@@ -1,3 +1,12 @@
 export const environment = {
-  production: true
+  production: true,
+  firebaseConfig:  {
+    apiKey: "AIzaSyDt_Vtb3GDIfLfCAt9Z60bw3ItUdBQ-XLg",
+    authDomain: "jcautopartes.firebaseapp.com",
+    projectId: "jcautopartes",
+    storageBucket: "jcautopartes.appspot.com",
+    messagingSenderId: "830834835021",
+    appId: "1:830834835021:web:ea3261f91e3151e6cfe604",
+    measurementId: "G-NZJBSZ3TWM"
+  }
 };
--- jcautopartes/src/environments/environment.ts
+++ jcautopartes/src/environments/environment.ts
@@ -3,7 +3,16 @@
 // The list of file replacements can be found in `angular.json`.
 
 export const environment = {
-  production: false
+  production: false,
+  firebaseConfig:  {
+    apiKey: "AIzaSyDt_Vtb3GDIfLfCAt9Z60bw3ItUdBQ-XLg",
+    authDomain: "jcautopartes.firebaseapp.com",
+    projectId: "jcautopartes",
+    storageBucket: "jcautopartes.appspot.com",
+    messagingSenderId: "830834835021",
+    appId: "1:830834835021:web:ea3261f91e3151e6cfe604",
+    measurementId: "G-NZJBSZ3TWM"
+  }
 };
 
 /*
